59445ba072365796bc47f7776e52614526c1a6a2
John Santosuosso <john.santosuosso1@gmail.com>
2022-07-23 19:28:43 -0400

    update practice

--- lib/activerecord_practice.rb
+++ lib/activerecord_practice.rb
@@ -17,11 +17,40 @@ class Customer < ActiveRecord::Base
   #  You should NOT need to call Ruby library functions for sorting, filtering, etc.
 
   def self.any_candice
-    # YOUR CODE HERE to return all customer(s) whose first name is Candice
-    # probably something like:  Customer.where(....)
+    Customer.where('first = ?', "Candice")
   end
+
   def self.with_valid_email
-    # YOUR CODE HERE to return only customers with valid email addresses (containing '@')
+    Customer.where('email LIKE ?', '%@%')
   end
-  # etc. - see README.md for more details
+  
+  def self.with_dot_org_email
+     Customer.where('email LIKE ?', '%.org%')
+  end
+
+  def self.with_invalid_email
+    Customer.where.not('email LIKE ?', '%@%')
+  end
+
+  def self.with_blank_email
+    Customer.where(email: [nil,""])
+  end
+
+  def self.born_before_1980
+    Customer.where('birthdate < ?', '1980-01-01')
+  end
+
+  def self.with_valid_email_and_born_before_1980
+    Customer.where('email LIKE ?', '%@%').where('birthdate < ?', '1980-01-01')
+  end
+
+  def self.last_names_starting_with_b
+    Customer.where('last LIKE ?', 'b%' ).order('birthdate asc')
+  end
+
+  def self.twenty_youngest
+    Customer.order('birthdate desc').limit(20)
+  end
+
+  
 end
